736d66c52c3d6611b5a1f1999c13755376e6dee6
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-04 13:13:31 +0300

    balanced brackets

--- AlgoExpert/balanced_brackets.cpp
+++ AlgoExpert/balanced_brackets.cpp
@@ -1,3 +1,17 @@
 //
 // Created by masinde mtesigwa on 4.7.2023.
 //
+#include <stack>
+#include <algorithm>
+using namespace  std;
+
+bool balancedBrackets(string str) {
+    stack<char> stack1;
+    stack1.push_back('@');
+
+    for (char c:str) {
+        if (c == '(' || c == '{' || c == '[' ){
+            stack1.push_back(c);
+        }
+    }
+}

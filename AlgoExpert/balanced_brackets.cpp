56ec8f9536205d18d8e6bcf4f5e4f7560251eb80
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-04 13:56:02 +0300

    balanced brackets

--- AlgoExpert/balanced_brackets.cpp
+++ AlgoExpert/balanced_brackets.cpp
@@ -13,5 +13,14 @@ bool balancedBrackets(string str) {
         if (c == '(' || c == '{' || c == '[' ){
             stack1.push_back(c);
         }
+        else if ( c == ')' || c == '}' || c == ']'){
+            if(!stack1.empty() && ((( c == ')') && (stack1.top() == '(')) || ((c == '}') && (stack1.top() == '{'))
+            || ((c == ']') && (stack1.top() == '[')))){
+                stack1.pop();
+            }
+            else
+                return false;
+        }
     }
+    return stack1.top() == '@';
 }

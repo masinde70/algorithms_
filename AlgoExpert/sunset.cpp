0a46b8bfa8025f34840c0060da926d4c8754ecce
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-05 09:08:28 +0300

    stacks

--- AlgoExpert/sunset.cpp
+++ AlgoExpert/sunset.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 
 // O(n) time | O(n) space - where n is the length of the input array
+
+vector<int> sunsetViews(vector<int> buildings, string direction){
+    vector<int> candidateBuildings;
+
+    int startIdx = buildings.size() - 1;
+    int step = -1;
+
+}

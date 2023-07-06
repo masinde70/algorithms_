87c10d8102702b4fbd009932420c3da858a2263a
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-06 09:20:20 +0300

    best digit

--- AlgoExpert/bestdigit.cpp
+++ AlgoExpert/bestdigit.cpp
@@ -3,3 +3,14 @@
 using namespace std;
 
 // O(n) time | O(n) space
+string bestDigits(string number, int numDigits){
+    stack<char> stack;
+
+    for(auto character: number) {
+        while(numDigits > 0 && !stack.empty() && character > stack.pop()){
+            numDigits--;
+            stack.pop();
+        }
+        stack.push(character);
+    }
+}

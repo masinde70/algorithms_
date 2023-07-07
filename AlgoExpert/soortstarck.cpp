98d8cac84bb0b3a22fa25e5a15b0b5c3f2f6cb99
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-07 13:07:34 +0300

    sort stack

---
+++ AlgoExpert/soortstarck.cpp
@@ -0,0 +1,5 @@
+/*Write a function that takes in an array of integers representing a stack, recursively sorts the stack in place
+ * (i.e doesn't create new array), and returns it.
+ * The array must be treated as stack, with the end of the array.
+ *
+ * */

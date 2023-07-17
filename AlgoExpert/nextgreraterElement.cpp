5042f5adae1d3adc7e35c78fd1c129d35ce857c8
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-17 09:15:14 +0300

    next greater element

--- AlgoExpert/nextgreraterElement.cpp
+++ AlgoExpert/nextgreraterElement.cpp
@@ -3,3 +3,7 @@
 using namespace std;
 
 // O(n) time | O(n) space -where n us the length of the array
+vector<int> nextGreaterElement(vector<int> array){
+    vector<int> result(array.size(), -1);
+    vector<int> stack;
+}

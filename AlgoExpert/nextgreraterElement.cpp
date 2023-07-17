75e75ae240ab159fe40c9eddd12b92d3284a9053
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-17 09:39:10 +0300

    next greater element

--- AlgoExpert/nextgreraterElement.cpp
+++ AlgoExpert/nextgreraterElement.cpp
@@ -1,3 +1,9 @@
+/*Write a function that takes in an array of integers and returns a new array containing, at each index, the next element
+ * in the input array that's greater than the element
+ * */
+
+
+
 #include <vector>
 
 using namespace std;
@@ -6,4 +12,18 @@ using namespace std;
 vector<int> nextGreaterElement(vector<int> array){
     vector<int> result(array.size(), -1);
     vector<int> stack;
+
+    for (int index = 0; index < 2 * array.size(); ++index){
+        int circularIndex = index % array.size();
+
+        while (stack.size() > 0 &&
+                array[stack[stack.size() - 1]] < array[circularIndex]) {
+            int top = stack.back();
+            stack.pop_back();
+            result[top] = array[circularIndex];
+        }
+        stack.push_back(circularIndex);
+    }
+
+    return result;
 }

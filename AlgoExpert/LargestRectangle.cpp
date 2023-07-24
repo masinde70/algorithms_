2fa6fec1f3d90f21b77eddd8347316948ef959f9
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-24 09:24:13 +0300

    Largest Rectangle UnderSkyline

--- AlgoExpert/LargestRectangle.cpp
+++ AlgoExpert/LargestRectangle.cpp
@@ -1,7 +1,8 @@
 //
 // Created by masinde mtesigwa on 24.7.2023.
 //
-/*
+/*  Largest Rectangle Under Skyline
+ *
  * Largest Rectangle that takes in an array of positive integers representing the heights of adjacent
  *buildings and returns the area of the largest rectangle that can be created by any number of adjacent buildings,
  * including just one building. Note that all buildings have the same width of 1 unit.

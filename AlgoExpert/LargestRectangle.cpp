26373130c7df0c0b95f2f8d722e0bd3e0f3f4124
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-24 09:21:04 +0300

    Largest Rectangle UnderSkyline

--- AlgoExpert/LargestRectangle.cpp
+++ AlgoExpert/LargestRectangle.cpp
@@ -6,8 +6,8 @@
  *buildings and returns the area of the largest rectangle that can be created by any number of adjacent buildings,
  * including just one building. Note that all buildings have the same width of 1 unit.
  *
- * For example, given buildings = [2, 1, 2], the area of the largest rectangle that can be created is 3, using all three buildings.
- * Since the minimum height of the three buildings is 1, you can create a rectangle that has height
+ * For example, given buildings = [2, 1, 2], the area of the largest rectangle that can be created is 3, using all
+ * three buildings.  Since the minimum height of the three buildings is 1, you can create a rectangle that has height
  * of 1 and width of 3 (the number of buildings). You could also create rectangles of area of 2 by using only
  * the first building or the last building, but these are clearly wouldn't be the largest rectangles.
  * */
